80ee5a41b5b3ad2bca44a3c385ae4264d39d25ff
alex garag <alex.garag@bk.ru>
2024-07-18 15:52:34 +0300

    копирование взлетело (упрощенное)

--- src/by/tms/lesson14/copyfile/Executor.java
+++ src/by/tms/lesson14/copyfile/Executor.java
@@ -1,11 +1,15 @@
 package by.tms.lesson14.copyfile;
 
+//import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Executor {
     public static void main(String[] args) {
 
-        String fileBeingCopied = "textFrom.txt";
+        String fileBeingCopied = "C:\\Users\\123\\IdeaProjects\\Lesson-14-c30-onl-CopyFile\\file.txt";
         boolean isApproved = false;
         Scanner scanner = new Scanner(System.in);
 
@@ -19,7 +23,22 @@ public class Executor {
 
         scanner.close();
 
-        System.out.println(nameFile);
+        StringBuilder stringBuilder = new StringBuilder();
+        try(FileReader fileReader = new FileReader(fileBeingCopied);
+            FileWriter fileWriter = new FileWriter(nameFile)) {
+            while (fileReader.ready()) {
+                stringBuilder.append((char)fileReader.read());
+            }
+            fileWriter.write(stringBuilder.toString());
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // проверить через хэш-функцию
+        boolean isCopyingSuccessful = false;
+        if (isCopyingSuccessful) System.out.println("Результат копироания: успешно");
+        else  System.out.println("Результат копирования: не удачно");
 
     }
 }
