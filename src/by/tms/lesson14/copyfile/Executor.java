0478736f7ad0f6c8f37d0ecdaa08cdc9f87bb0a7
alex garag <alex.garag@bk.ru>
2024-07-18 16:23:52 +0300

    хэш-функция для проверки взлетела

--- src/by/tms/lesson14/copyfile/Executor.java
+++ src/by/tms/lesson14/copyfile/Executor.java
@@ -1,44 +1,48 @@
 package by.tms.lesson14.copyfile;
 
-//import java.io.FileNotFoundException;
+import by.tms.lesson14.copyfile.service.GetterHash;
+
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
 import java.util.Scanner;
 
 public class Executor {
     public static void main(String[] args) {
 
-        String fileBeingCopied = "C:\\Users\\123\\IdeaProjects\\Lesson-14-c30-onl-CopyFile\\file.txt";
+        String original = "C:\\Users\\123\\IdeaProjects\\Lesson-14-c30-onl-CopyFile\\file.txt";
         boolean isApproved = false;
         Scanner scanner = new Scanner(System.in);
 
-        // аналог    ConsoleEmployee.suggestCopying(fileBeingCopied) см. ExecutorStatic
-        System.out.print("Вы хотите произвести копирование файла \"" + fileBeingCopied + "\"? Д/Н (Y/N) ");
+        // аналог    ConsoleEmployee.suggestCopying(original) см. ExecutorStatic
+        System.out.print("Вы хотите произвести копирование файла \"" + original + "\"? Д/Н (Y/N) ");
         char letter = scanner.nextLine().toLowerCase().charAt(0);
-
+//todo отработать отказ от копирования
         // аналог    ConsoleEmployee.acceptFileName() см. ExecutorStatic
         System.out.print("Введите имя файла-копии: ");
-        String nameFile = scanner.next();
+        String copy = scanner.next();
 
         scanner.close();
 
         StringBuilder stringBuilder = new StringBuilder();
-        try(FileReader fileReader = new FileReader(fileBeingCopied);
-            FileWriter fileWriter = new FileWriter(nameFile)) {
+        try (FileReader fileReader = new FileReader(original);
+             FileWriter fileWriter = new FileWriter(copy)) {
             while (fileReader.ready()) {
-                stringBuilder.append((char)fileReader.read());
+                stringBuilder.append((char) fileReader.read());
             }
             fileWriter.write(stringBuilder.toString());
-
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        // проверить через хэш-функцию
-        boolean isCopyingSuccessful = false;
-        if (isCopyingSuccessful) System.out.println("Результат копироания: успешно");
-        else  System.out.println("Результат копирования: не удачно");
+        // проверка правильности копирования через хэш-функцию
+        try {
+            if (GetterHash.getHash(original).equals(GetterHash.getHash(copy))) System.out.println("Результат копирования: успешно");
+            else System.out.println("Результат копирования: не удачно");
+        } catch (IOException | NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
---
+++ src/by/tms/lesson14/copyfile/service/GetterHash.java
@@ -0,0 +1,31 @@
+package by.tms.lesson14.copyfile.service;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class GetterHash {
+    public static String getHash(String fileName) throws IOException, NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        FileInputStream fis = new FileInputStream(fileName);
+
+        byte[] dataBytes = new byte[1024];
+
+        int nread = 0;
+        while ((nread = fis.read(dataBytes)) != -1) {
+            md.update(dataBytes, 0, nread);
+        }
+        ;
+        byte[] mdbytes = md.digest();
+
+        //convert the byte to hex format
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < mdbytes.length; i++) {
+            sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
+        }
+
+        fis.close();
+        return sb.toString();
+    }
+}
